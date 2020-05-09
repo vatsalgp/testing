f40678e1eb6ae9bae8ccf501018b65a45d9b8eae
Vatsal Gupta <vatsalgupta280@gmail.com>
2020-05-09 14:21:51 +0530

    added root node path

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,12 @@
 import React from "react";
-import CommentBox from "./CommentBox";
-import CommentList from "./CommentList";
+import CommentBox from "CommentBox";
+import CommentList from "CommentList";
 
-const App = () => {
-    return (
-        <div>
-            <CommentBox />
-            <CommentList />
-        </div>
-    );
-};
+const App = () => (
+    <div>
+        <CommentBox />
+        <CommentList />
+    </div>
+);
 
 export default App;
